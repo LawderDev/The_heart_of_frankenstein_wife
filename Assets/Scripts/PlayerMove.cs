11d23ea78c73edbe724ea9bb2302fc33fae93e15
Kenny Ramin <kramin@cremi.emi.u-bordeaux1.fr>
2023-10-21 11:25:18 +0200

    add double jump

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-   [SerializeField] private float speed;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private int maxJumps = 2; // Nombre maximal de sauts
+
     private Rigidbody2D body;
- 
+    private int jumpCount = 0;
+
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
     }
- 
+
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < maxJumps)
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpForce);
+            jumpCount++;
+        }
+
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
- 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            jumpCount = 0; // Réinitialiser le compteur de sauts lorsque le joueur touche le sol
+        }
     }
 }
